07fcfb0a4ea34f829aee6363efb8fe057b46af9d
abdtamimi98 <abdaltamimi1998@outlook.com>
2020-06-30 22:08:45 +0300

    .

---
+++ HR_Alpha_v0.2/HR_Alpha_v0.2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HR_Alpha_v0._2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
