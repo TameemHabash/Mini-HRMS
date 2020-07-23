6884a0c20d07b603a707fcc0f96e1802bb8d2d97
Tameem HA <tameem.habash@gmail.com>
2020-07-23 16:58:40 +0300

    initiated deparment service

--- src/app/services/department.service.ts
+++ src/app/services/department.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
+import { DepartmentsPageComponent } from '../in-app/departments-page/departments-page.component';
+import { Department } from '../models/department';
 
 @Injectable({
   providedIn: 'root'
 })
-export class DepartmentService {
+export class DepartmentService implements OnInit {
+  private departments: Department[];
 
   constructor() { }
+  ngOnInit() {
+    this.departments = [
+      new Department(1, 'programming', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis consectetur deserunt fuga aperiam sequi rem molestias provident obcaecati doloribus'),
+      new Department(2, 'QA', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis consectetur deserunt fuga aperiam sequi rem molestias provident obcaecati doloribus'),
+      new Department(3, 'deplooying', 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis consectetur deserunt fuga aperiam sequi rem molestias provident obcaecati doloribus')
+    ]
+  }
+  getDepartments() {
+    return this.departments.slice();
+  }
+  createDepartment(id: number, name: string, description: string) {
+    const newDepartment = new Department(id, name, description);
+    this.departments.push(newDepartment);
+    return this.getDepartments();
+  }
 }
