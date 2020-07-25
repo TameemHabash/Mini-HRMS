57adf3c459d38b212e47ab2d56c750435802d242
Tameem HA <tameem.habash@gmail.com>
2020-07-25 16:54:10 +0300

    added dialog to departments page component

--- src/app/in-app/departments-page/departments-page.component.ts
+++ src/app/in-app/departments-page/departments-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, AfterContentInit } from '@angular/core';
 import { DepartmentService } from 'src/app/services/department.service';
 import { Department } from 'src/app/models/department';
+import { MatDialog } from '@angular/material/dialog';
+import { DepartmentDialogComponent } from './department-dialog/department-dialog.component';
 @Component({
   selector: 'app-departments-page',
   templateUrl: './departments-page.component.html',
@@ -8,11 +10,15 @@ import { Department } from 'src/app/models/department';
 })
 export class DepartmentsPageComponent implements AfterContentInit {
   departments: Department[];
-  constructor(private departmentsServ: DepartmentService) { }
+  constructor(private departmentsServ: DepartmentService, private dialog: MatDialog) { }
 
   ngAfterContentInit(): void {
     this.departments = this.departmentsServ.getDepartments();
   }
-
-
+  onAddDepartment() {
+    this.dialog.open(DepartmentDialogComponent);
+  }
+  onShowDepartmentDetails(department) {
+    this.dialog.open(DepartmentDialogComponent, { data: department });
+  }
 }
