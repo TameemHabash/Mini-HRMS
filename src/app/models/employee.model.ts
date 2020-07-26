240ced053c986f9e2fb60e267a2ae3c8ada12b48
Tameem HA <tameem.habash@gmail.com>
2020-07-26 19:07:32 +0300

    added attributes of employee model

--- src/app/models/employee.model.ts
+++ src/app/models/employee.model.ts
@@ -1,3 +1,24 @@
 export class Employee {
-    constructor() { }
+    constructor(
+        private _ID: number,
+        public name: string,
+        public gender: string,
+        public SSN: string,
+        public telNumber: string,
+        public nationality: string,
+        public nationalID: string,
+        public birthDate: string,
+        public startDate: string,
+        public rating: number,
+        public address: string,
+        public status: string,
+        public email: string,
+        public departmentID: number,
+        public sectorID: number,
+        public HRID: number
+    ) { }
+
+    get ID() {
+        return this._ID;
+    }
 }
