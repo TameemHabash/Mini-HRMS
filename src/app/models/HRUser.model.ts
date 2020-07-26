2a095e06c483270b1cbb14664aa98a462ce9872d
Tameem HA <tameem.habash@gmail.com>
2020-07-26 19:58:02 +0300

    added attributes of HRUser model

--- src/app/models/HRUser.model.ts
+++ src/app/models/HRUser.model.ts
@@ -1,3 +1,24 @@
 export class HRUser {
-    constructor() { }
+    constructor(
+        private _HRID: number,
+        private _userName: string,
+        private _password: string,
+        private _email: string,
+        private _image: string
+    ) { }
+    get HRID() {
+        return this._HRID;
+    }
+    get userName() {
+        return this._userName;
+    }
+    get password() {
+        return this._password;
+    }
+    get email() {
+        return this._email;
+    }
+    get image() {
+        return this._image;
+    }
 }
