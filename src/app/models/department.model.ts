eab4396d7fb25d39b80c936a8c91c15d2010d68e
Tameem HA <tameem.habash@gmail.com>
2020-07-26 19:30:02 +0300

    added attributes of department model

--- src/app/models/department.model.ts
+++ src/app/models/department.model.ts
@@ -1,3 +1,35 @@
 export class DepartmentModel {
-    constructor() { }
+    constructor(
+        private _ID: number,
+        private _name: string,
+        private _description: string,
+        private _managerID: number
+    ) { }
+    get ID() {
+        return this._ID;
+    }
+    get name() {
+        return this._name;
+    }
+    get description() {
+        return this._description;
+    }
+    get managerID() {
+        return this._managerID;
+    }
+    set name(newName: string) {
+        if (newName !== '') {
+            this._name = newName;
+        }
+    }
+    set description(newDescription: string) {
+        if (newDescription !== '') {
+            this._description = newDescription;
+        }
+    }
+    set managerID(newManagerID: number) {
+        if (newManagerID !== -1) {
+            this._managerID = newManagerID;
+        }
+    }
 }
