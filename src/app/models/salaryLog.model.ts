25e5ae2e0a3ffe5a269b9a365bc564549d6cb1ce
Tameem HA <tameem.habash@gmail.com>
2020-08-15 19:20:20 +0300

    fixed salary log attribute names

--- src/app/models/salaryLog.model.ts
+++ src/app/models/salaryLog.model.ts
@@ -1,9 +1,9 @@
 export class SalaryLog {
 
-    constructor(private _ID: number, private _logDate: Date, private _salaryID: number, public value: number, public bonus?: number) { }
+    constructor(private _logID: number, private _logDate: Date, private _salaryID: number, public value: number, public bonus?: number) { }
 
     get ID() {
-        return this._ID;
+        return this._logID;
     }
     get salaryID() {
         return this._salaryID;
